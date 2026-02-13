8ec1a03d272a1dcb1da7f821c821dcc6cf0159ec
Ayush Kumar Singh <aj945892@gmail.com>
2026-02-13 13:36:24 +0530

    Add localStorage for tasks

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let inputField = document.getElementById('inputField');
 let addBtn = document.getElementById('addBtn');
 let taskList = document.querySelector('.task-list');
+let tasks = [];
+
+function createTaskElement(taskText){
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <span>${taskText}</span>
+    <button class="delete-btn">X</button>`
+
+    taskList.append(li);
+    inputField.value = "";
+}
 
 addBtn.addEventListener("click", ()=>{
     const inputVal = inputField.value.trim();
@@ -9,17 +20,30 @@ addBtn.addEventListener("click", ()=>{
         return;
     }
 
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <span>${inputVal}</span>
-    <button class="delete-btn">X</button>`
-
-    taskList.append(li);
-    inputField.value = "";
+    createTaskElement(inputVal);
+    tasks.push(inputVal);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    
 });
 
 taskList.addEventListener("click", (e)=>{
     if(e.target.classList.contains("delete-btn")){
+        const taskText = e.target.previousElementSibling.textContent;
+
+        tasks = tasks.filter(task => task !== taskText);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        
         e.target.parentElement.remove();
+    }
+});
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if(storedTasks){
+        tasks=storedTasks;
+        tasks.forEach(task=>{
+            createTaskElement(task);
+        });
     }
 });
