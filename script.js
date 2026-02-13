d397d0d3a3be9233a1f9ef5eed17ad669322fcaf
Ayush Kumar Singh <aj945892@gmail.com>
2026-02-13 15:12:42 +0530

    Add mark as complete for tasks

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let tasks = [];
 
 function createTaskElement(task){
     const li = document.createElement('li');
+
+    if(task.completed){
+        li.classList.add("completed");
+    }
+
     li.innerHTML = `
-    <span>${task.text}</span>
+    <span class="task-text">${task.text}</span>
     <button class="delete-btn">X</button>`;
 
     li.setAttribute("data-id", task.id);
@@ -23,7 +28,8 @@ addBtn.addEventListener("click", ()=>{
 
     const taskObj = {
         id: Date.now(),
-        text: inputVal
+        text: inputVal,
+        completed: false
     };
     createTaskElement(taskObj);
     tasks.push(taskObj);
@@ -38,14 +44,27 @@ inputField.addEventListener("keypress", (e)=>{
 })
 
 taskList.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("delete-btn")){
-        const li = e.target.parentElement;
-        const taskId = Number(li.getAttribute("data-id"));
 
+    const li = e.target.parentElement;
+    const taskId = Number(li.getAttribute("data-id"));
+
+    //DELETE
+    if(e.target.classList.contains("delete-btn")){
         tasks = tasks.filter(task => task.id !== taskId);
         localStorage.setItem("tasks", JSON.stringify(tasks));
-        
         li.remove();
+    }
+
+    //TOGGLE
+    if(e.target.classList.contains("task-text")){
+        tasks = tasks.map(task =>{
+            if(task.id === taskId){
+                task.completed = !task.completed;
+            }
+            return task;
+        });
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        li.classList.toggle("completed");
     }
 });
 
